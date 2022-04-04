d78d4ff44a6437868f61ed1d5fdc51eb7ba126f4
Remi MERGEN <remi.mergen@epitech.eu>
2022-04-04 22:48:44 +0200

    [ADD] Makefile

---
+++ usefull.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** chocolatine
+** File description:
+** usefull
+*/
+
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    printf("That's me !\n");
+    return 0;
+}
