d2b7b4663d1c89b854929653a193d3e8731fea60
Remi MERGEN <remi.mergen@epitech.eu>
2022-04-06 02:20:17 +0200

    setup just a printf and test like 84 == 84

--- Makefile
+++ Makefile
@@ -7,49 +7,28 @@
 
 CC			=	gcc
 
-SRC			=	src/get_map_tool.c				\
-				src/auxiliary_coding_style.c	\
-				src/bsq.c						\
-				src/start_bsq.c					\
-				src/main.c						\
+SRC			=	usefull.c
 
 OBJ			=	$(SRC:.c=.o)
 
-NAME		=	bsq
+NAME		=	binary
 
-CPPFLAGS	=	-I./include/
-
-LDFLAGS		=	-L./lib
-
-LDLIBS		=	-lmy
-
-LIB			=	./lib/libmy.a
-
-$(NAME):	 $(LIB) $(OBJ)
-	$(CC) $(OBJ) -o $(NAME) $(CPPFLAGS) $(LDFLAGS) $(LDLIBS)
+$(NAME):	 $(OBJ)
+	$(CC) $(OBJ) -o $(NAME)
 
 all:	$(NAME)
 
-debug:	fclean $(LIB) $(OBJ)
-	$(CC) $(OBJ) -o $(NAME) $(CPPFLAGS) $(LDFLAGS) $(LDLIBS) -W -Wall -Wextra -g
-
-gdb:	fclean $(LIB) $(OBJ)
-	$(CC) $(OBJ) -o $(NAME) $(CPPFLAGS) $(LDFLAGS) $(LDLIBS) -W -Wall -Wextra -g
-	gdb ./$(NAME)
-
-tests_run: $(LIB)
+tests_run:
 	make -C tests/
-
-$(LIB):
-	make -C ./lib/my/
 
 clean:
 	rm -f $(OBJ)
-	make clean -C ./lib/my/
+
+coverage:
+	@gcovr
 
 fclean: clean
 	rm -f $(NAME)
-	make fclean -C ./lib/my
 	make fclean -C ./tests
 
 re:	fclean all
--- tests/test_bsq.c
+++ tests/test_bsq.c
@@ -7,69 +7,33 @@
 
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
-#include "bsqh.h"
 
-Test(bsq, incorrect_file)
+Test(bsq, test1)
 {
-    cr_assert_eq(bsq("azertyuiop"), 84);
+    cr_assert_eq(84, 84);
 }
 
-Test(bsq, too_long_line)
+Test(bsq, test2)
 {
-    cr_assert_eq(bsq("map1"), 84);
+    cr_assert_eq(84, 84);
 }
 
-Test(bsq, too_short_line)
+Test(bsq, test3)
 {
-    cr_assert_eq(bsq("map11"), 84);
+    cr_assert_eq(84, 84);
 }
 
-Test(bsq, too_much_lines)
+Test(bsq, test4)
 {
-    cr_assert_eq(bsq("map2"), 84);
+    cr_assert_eq(84, 84);
 }
 
-Test(bsq, too_few_lines)
+Test(bsq, test5)
 {
-    cr_assert_eq(bsq("map21"), 84);
+    cr_assert_eq(84, 84);
 }
 
-Test(bsq, invalid_chars)
+Test(bsq, test6)
 {
-    cr_assert_eq(bsq("map3"), 84);
-}
-
-Test(bsq, correct_map)
-{
-    cr_redirect_stdout();
-    bsq("map");
-    cr_assert_stdout_eq_str(".oxx.\no.xxo\n.....\n.oo.o\n..o.o\n");
-}
-
-Test(bsq, correct_empty_map)
-{
-    cr_redirect_stdout();
-    bsq("map_empty");
-    cr_assert_stdout_eq_str("xxxxx\nxxxxx\nxxxxx\nxxxxx\nxxxxx\n");
-}
-
-Test(bsq, correct_empty_column_map)
-{
-    cr_redirect_stdout();
-    bsq("map_empty_column");
-    cr_assert_stdout_eq_str("x....\n");
-}
-
-Test(bsq, correct_empty_column_line)
-{
-    cr_redirect_stdout();
-    bsq("map_empty_line");
-    cr_assert_stdout_eq_str("x\n.\n.\n.\n.\n");
-}
-
-Test(bsq, correct_filled_map)
-{
-    cr_redirect_stdout();
-    bsq("map_filled");
-    cr_assert_stdout_eq_str("ooooo\nooooo\nooooo\nooooo\nooooo\n");
+    cr_assert_eq(84, 84);
 }
