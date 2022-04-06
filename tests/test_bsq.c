1a431aaedaef0e6fc466705f1d36f5ecbc259b94
Remi-Mergen <91549087+Remi-Mergen@users.noreply.github.com>
2022-04-06 02:39:32 +0200

    Easy test (#3)
    * [ADD] usefull.c
    
    * [ADD] Makefile
    
    * [ADD] if exit
    
    * echo
    
    * rm ;
    
    * changing condition
    
    * add [
    
    * rm - in on
    
    * go mettre branch ignore
    
    * echo var
    
    * add list
    
    * test
    
    * init mirror repo
    
    * test new loop
    
    * try
    
    * try something else and add fetch-depth: 0
    
    * new private key
    
    * [ADD] BSQ file for tests
    
    * [FIX] returned private_key instead of public_key
    
    * [FIX] binary name
    
    * [ADD] map in argument
    
    * [RM] useless files
    
    * setup just a printf and test like 84 == 84
    
    * [ADD] print.c
    
    * push to pull

---
+++ tests/test_bsq.c
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2019
+** test_init_op
+** File description:
+** infinadd criterion test
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+Test(bsq, test1)
+{
+    cr_assert_eq(84, 84);
+}
+
+Test(bsq, test2)
+{
+    cr_assert_eq(84, 84);
+}
+
+Test(bsq, test3)
+{
+    cr_assert_eq(84, 84);
+}
+
+Test(bsq, test4)
+{
+    cr_assert_eq(84, 84);
+}
+
+Test(bsq, test5)
+{
+    cr_assert_eq(84, 84);
+}
+
+Test(bsq, test6)
+{
+    cr_assert_eq(84, 84);
+}
